89fe89103bdc1a91ae7e8f79400d186042c6e91f
machacamoya <85935419+machacamoya@users.noreply.github.com>
2021-08-04 16:25:25 +0200

    Update pruebas.py

--- src/pruebas.py
+++ src/pruebas.py
@@ -1,4 +1,5 @@
 from flask import Flask, request
+from time import strftime
 
 print("#######################")
 print(__name__)
@@ -8,11 +9,18 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
+    with open("/log/pcm.log","a") as fp:
+        fp.write( "Fecha y hora " + strftime("%c"))
+        fp.write('\t- Hello, World!\n')
     return 'Hello, World!'
+
 @app.route('/p')
 def parametros():
     par1 = request.args.get('par1','ninguno')
     par2 = request.args.get('par2','ninguno')
+    with open("/log/pcm.log","a") as fp:
+        fp.write( "Fecha y hora " + strftime("%c"))
+        fp.write('\t- Se ha introducido.............{}--{}\n'.format(par1, par2))
     return 'Se ha introducido.............{}--{}'.format(par1, par2)
 
 if __name__ == '__main__':
