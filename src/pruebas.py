8a308716b02c7f83daf63198aaf3726a97e0df22
machacamoya <85935419+machacamoya@users.noreply.github.com>
2021-08-06 10:40:52 +0200

    Update pruebas.py

--- src/pruebas.py
+++ src/pruebas.py
@@ -1,5 +1,6 @@
 from flask import Flask, request
 from time import strftime
+from os import getenv
 
 print("#######################")
 print(__name__)
@@ -12,6 +13,8 @@ def hello():
     with open("/log/pcm.log","a") as fp:
         fp.write( "Fecha y hora " + strftime("%c"))
         fp.write('\t- Hello, World!\n')
+        if getenv('PCM'):
+            fp.write(getenv('PCM' + '\n')
     return 'Hello, World!'
 
 @app.route('/p')
@@ -21,6 +24,8 @@ def parametros():
     with open("/log/pcm.log","a") as fp:
         fp.write( "Fecha y hora " + strftime("%c"))
         fp.write('\t- Se ha introducido.............{}--{}\n'.format(par1, par2))
+        if getenv('PCM'):
+            fp.write(getenv('PCM' + '\n')
     return 'Se ha introducido.............{}--{}'.format(par1, par2)
 
 if __name__ == '__main__':
